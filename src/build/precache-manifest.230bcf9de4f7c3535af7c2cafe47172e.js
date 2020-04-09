a5c9912e5ae298a1e01a616aad86ce903d6b6f29
Anupam Govind <agovind@AJM07.local>
2020-04-09 18:13:36 -0500

    React Project

---
+++ src/build/precache-manifest.230bcf9de4f7c3535af7c2cafe47172e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b998cec74797656bcad4fad66c394378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e1a9a1a33441efadc3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "03d6dfdb49e1f0428b3f",
+    "url": "/static/js/2.1fc4d83f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fc4d83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e1a9a1a33441efadc3",
+    "url": "/static/js/main.076f7a54.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
